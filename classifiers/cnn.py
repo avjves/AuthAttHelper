d2290f84d06b6054c0b3ca2234ef083f79ebb05a
Aleksi <avjves@utu.fi>
2018-04-26 12:25:19 +0300

    CNN model

--- classifiers/cnn.py
+++ classifiers/cnn.py
@@ -4,6 +4,7 @@ np.random.seed(1)
 from keras.models import Sequential, Model
 from keras.layers import Dense, Dropout, Activation, Merge, Input, merge, Flatten,ActivityRegularization,Conv1D,GlobalMaxPooling1D
 from keras.layers.embeddings import Embedding
+from keras.layers.merge import Concatenate
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.optimizers import Adam
 
@@ -26,7 +27,7 @@ class CNN:
 		self.level = "char"
 
 
-	def make_model(self, verbose=True, weights=None):
+	def make_model_old(self, verbose=True, weights=None):
 		input_layer = Input(shape=(self.max_length,), name="input_ngrams_{}".format(self.ngram), dtype="int32")
 		embedding = Embedding(len(self.vocab), self.vector_size, input_length=self.max_length)(input_layer)
 		conv = Conv1D(self.vector_size, 5, padding="same", activation="relu")(embedding)
@@ -40,6 +41,28 @@ class CNN:
 			model.summary()
 		return model
 
+	def make_model(self, verbose=True):
+		input_layer = Input(shape=(self.max_length,), dtype="int32")
+		embeddings = Embedding(len(self.vocab), 200, input_length=self.max_length)(input_layer)
+		conv_3 = Conv1D(200, 3, activation="relu")(Conv1D(400, 3, activation="relu")(embeddings))
+		conv_4 = Conv1D(200, 3, activation="relu")(Conv1D(400, 4, activation="relu")(embeddings))
+		conv_5 = Conv1D(200, 3, activation="relu")(Conv1D(400, 5, activation="relu")(embeddings))
+		pool_3 = GlobalMaxPooling1D()(conv_3)
+		pool_4 = GlobalMaxPooling1D()(conv_4)
+		pool_5 = GlobalMaxPooling1D()(conv_5)
+		conc = Concatenate(axis=1)([pool_3, pool_4, pool_5])
+		drop = Dropout(0.25)(conc)
+		dense = Dense(150, activation="tanh")(drop)
+		out = Dense(1, activation="sigmoid")(dense)
+
+		model = Model(inputs=[input_layer], outputs=[out])
+		optimizer = Adam(lr=0.0001)
+		model.compile(optimizer=optimizer, metrics=["accuracy"], loss="binary_crossentropy")
+		if verbose:
+			model.summary()
+		return model
+
+
 
 	def ngram_generator(self, text, level, ngram_length, max_length=1000000000):
 		if level == "word":
@@ -50,8 +73,9 @@ class CNN:
 
 	def read_vocabulary(self, data):
 		vocab = {}
-		vocab["<NOTFOUND>"] = 0
-		feat_id = 1
+		vocab["<MASK>"] = 0
+		vocab["<NOTFOUND>"] = 1
+		feat_id = 2
 		for text in data:
 			for ngram in self.ngram_generator(text, self.level, self.ngram):
 				if ngram not in vocab:
@@ -64,7 +88,7 @@ class CNN:
 		y = np.zeros((len(X_data), 1), np.int) ## 2 classes == binary
 		for row, text in enumerate(X_data):
 			for column, ngram in enumerate(self.ngram_generator(text, self.level, self.ngram, self.max_length)):
-				X[row, column] = self.vocab.get(ngram, 0)
+				X[row, column] = self.vocab.get(ngram, 1)
 			y[row] = info[row][3]
 		return X, y
 
@@ -115,14 +139,14 @@ class CNN:
 			X_test, y_test, test_info = shuffle(X_test, y_test, test_info, random_state=1)
 
 			self.vocab = self.read_vocabulary(X_train)
-			self.model = self.make_model(verbose=False)
+			self.model = self.make_model(verbose=True)
 
 			X_train, y_train = self.matrixify(X_train, y_train, train_info)
 			X_test, y_test = self.matrixify(X_test, y_test, test_info)
 
 			cb = EarlyStopping(monitor="val_acc", verbose=1, patience=10000)
 
-			res = self.model.fit(X_train, y_train, batch_size=self.minibatch_size, shuffle=False, validation_data=(X_test, y_test), epochs=epoch_count, callbacks=[cb])
+			res = self.model.fit(X_train, y_train, batch_size=self.minibatch_size, shuffle=True, validation_data=(X_test, y_test), epochs=epoch_count, callbacks=[cb])
 			results.append(res.history)
 			#print(res.history)
 		maxn = 0
@@ -154,13 +178,13 @@ class CNN:
 			X_train, y_train = self.matrixify(X_train, y_train, train_info)
 			X_test, y_test = self.matrixify(X_test, y_test, test_info)
 
-			es = EarlyStopping(monitor="val_acc", verbose=1, patience=400)
+			es = EarlyStopping(monitor="val_acc", verbose=1, patience=400) ## don't stop
 			model_path = "models/model_{}_{}.hdf5".format(self.ngram, iteration)
 			if not os.path.exists("models"):
 				os.makedirs("models")
 
 			mc = ModelCheckpoint(model_path, monitor="val_acc", verbose=1, save_best_only=True, mode="max")
-			self.model.fit(X_train, y_train, batch_size=self.minibatch_size, shuffle=False, validation_data=(X_test, y_test), epochs=self.epochs, callbacks=[es, mc])
+			self.model.fit(X_train, y_train, batch_size=self.minibatch_size, shuffle=True, validation_data=(X_test, y_test), epochs=self.epochs, callbacks=[es, mc])
 			#self.make_model(weights="models/model.hdf5")
 			self.model.load_weights(model_path)
 			prediction = self.model.predict(X_test)
